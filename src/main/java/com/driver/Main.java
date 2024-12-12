c992523413f4f4f9d124e518be9d9271b52594c5
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 21:48:15 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,9 +17,9 @@ public class Main {
 
   public static void main(String[] args){
     B obj1 = new B();
-    obj1.meth();
+    System.out.println(obj1.meth());
 
     B obj2 = new B();
-    obj2.meth();
+    System.out.println(obj2.meth());
   }
 }
